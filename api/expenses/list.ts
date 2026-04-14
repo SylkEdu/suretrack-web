ee4ac24591f3990f5be9d07b38d71c319b917805
Paulo Eduardo <pauloeduardodeandradesouza@gmail.com>
2026-04-14 17:00:40 -0300

    First commit

--- api/expenses/list.ts
+++ api/expenses/list.ts
@@ -1,5 +1,5 @@
-import { db } from "../../lib/firebaseAdmin";
-import { verifyToken } from "../../lib/authMiddleware";
+import { db } from "../../lib/firebaseAdmin.js";
+import { verifyToken } from "../../lib/authMiddleware.js";
 
 export default async function handler(req, res) {
   try {
--- api/operations/delete.ts
+++ api/operations/delete.ts
@@ -1,5 +1,5 @@
-import { db } from "../../lib/firebaseAdmin";
-import { verifyToken } from "../../lib/authMiddleware";
+import { db } from "../../lib/firebaseAdmin.js";
+import { verifyToken } from "../../lib/authMiddleware.js";
 
 export default async function handler(req, res) {
   if (req.method !== "DELETE") return res.status(405).end();
